6b57272cab0f17791fa1c132542cf7cb283b331b
Parth Joshi <parthjoshi.1618@gmail.com>
2017-09-20 22:35:33 -0500

    Added git url to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = "CodeChallenge",
-    version = "",
-    author = "Parth Joshi`",
-    author_email = "parthjoshi.1618@gmail.com",
-    description = "Python Challenge - Palindromic Numbers",
-    license = "",
-    keywords = "",
-    url = "",
+    name="CodeChallenge",
+    version="",
+    author="Parth Joshi`",
+    author_email="parthjoshi.1618@gmail.com",
+    description="Python Challenge - Palindromic Numbers",
+    license="",
+    keywords="",
+    url="https://github.com/parth1618/Palindrome",
     packages=['src', 'test'],
     classifiers=[
         "Development Status :: 3 - Alpha",
